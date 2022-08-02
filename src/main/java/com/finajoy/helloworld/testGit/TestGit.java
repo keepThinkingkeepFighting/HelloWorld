589df560df5a79a9701cf0239c2452c04172b9d0
hushuai <coderhushuai@163.com>
2022-08-02 16:07:21 +0800

    重演公司上次代码丢失的问题

---
+++ src/main/java/com/finajoy/helloworld/testGit/TestGit.java
@@ -0,0 +1,12 @@
+package com.finajoy.helloworld.testGit;
+
+/**
+ * 我在公司曾经犯过一个错误。
+ * 别人已经在A文件提交了东西，而我也在这个文件上修改了东西，于是有了冲突，然后我merge，merge到一半，我发现太多了，于是好像按了什么按钮
+ * 结果：玛德我写的代码全不见了
+ */
+public class TestGit {
+    public static void testA(){
+        System.out.println("something");
+    }
+}
