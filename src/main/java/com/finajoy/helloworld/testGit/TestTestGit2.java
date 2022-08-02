e998948485c59f8671a6a90969c573ca8fe0ae09
hushuai <coderhushuai@163.com>
2022-08-02 16:26:12 +0800

    B用户，删掉了代码，提交了一些东西，还把类名都改了

---
+++ src/main/java/com/finajoy/helloworld/testGit/TestTestGit2.java
@@ -0,0 +1,12 @@
+package com.finajoy.helloworld.testGit;
+
+
+public class TestTestGit2 {
+    /*
+    # 别让情绪成为性格缺陷
+
+        没有人会在意你快不快乐，痛不痛苦的原因，或者事情背后的真相，只会在意你有没有用。所以，不要做**煤气罐**，一点就炸。
+
+        体面的表达情绪，否则你的情绪将被视为性格缺陷，
+     */
+}
